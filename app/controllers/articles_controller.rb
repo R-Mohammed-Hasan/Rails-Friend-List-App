d3cc16dab023d30cd788808d351d8db42a24f0a5
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-06-20 14:47:58 +0530

    Added all files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+
+  layout "main"
+
+
+
+  def about
+
+    puts "This is about fn"
+
+  end
+
+
+
+  def home
+
+    puts "This is home"
+
+  end
+
+
+
+  # def tests
+
+
+
+  #   render "articles/about"
+
+
+
+  # end
+
+
+
+end
+
